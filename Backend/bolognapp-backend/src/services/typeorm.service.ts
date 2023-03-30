549a8dd13c22359f75a6f7fb36abb8f4203d9887
Andrija Eremic <andrija.eremic@gmail.com>
2023-03-30 22:54:05 +0200

    typeormconfig fix

--- Backend/bolognapp-backend/src/services/typeorm.service.ts
+++ Backend/bolognapp-backend/src/services/typeorm.service.ts
@@ -15,9 +15,10 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       database: this.config.get<string>('DATABASE_NAME'),
       username: this.config.get<string>('DATABASE_USER'),
       password: this.config.get<string>('DATABASE_PASSWORD'),
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      migrations: ['dist/migrations/*{.ts,.js}'],
+      entities: ['./dist/**/*.entity{.ts,.js}'],
+      migrations: ['./dist/migrations/*{.ts,.js}'],
       migrationsTableName: 'typeorm_migrations',
+      logging: true,
       autoLoadEntities: true,
       synchronize: true
     };
