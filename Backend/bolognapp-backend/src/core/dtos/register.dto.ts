943cbfdf4e7edbedf80b15a686985adf1f11c93b
Andrija Eremic <andrija.eremic@gmail.com>
2023-04-12 23:01:44 +0200

    changed imports for validationpipe

--- Backend/bolognapp-backend/src/core/dtos/register.dto.ts
+++ Backend/bolognapp-backend/src/core/dtos/register.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsNumber, IsString } from "@nestjs/class-validator";
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class RegisterDto {  
     @IsString()
