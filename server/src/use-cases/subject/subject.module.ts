d6c1efc0178cc477f12a3f6230e27d201ce448d3
andrija.eremic <jagodinagrobari@gmail.com>
2023-04-14 18:24:22 +0200

    import fix

--- server/src/use-cases/subject/subject.module.ts
+++ server/src/use-cases/subject/subject.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProfessorsSubjects, StudentsSubjects, Subject } from 'src/infrastructure/implementations/models';
+import { ProfessorsSubjects, StudentsSubjects, Subject } from 'src/infrastructure/data/models';
 import { SubjectUseCases } from './subject.use-case';
 import { SubjectController } from 'src/controllers/subject.controller';
 import { SubjectRepositoryAbstract } from 'src/core/abstracts/repositories/subject.repository.abstract';
-import { SubjectRepository } from 'src/infrastructure/implementations/repositories/subject.repository';
+import { SubjectRepository } from 'src/infrastructure/data/repositories/subject.repository';
 import { LoggerModule } from '../logger/logger.module';
 
 @Module({
