8ce8792d870416d42134136c4095712236607a7a
Andrija Eremic <andrija.eremic@gmail.com>
2023-06-07 23:03:52 +0200

    email verification minor fix

--- server/src/core/abstracts/repositories/email-verification.repository.abstract.ts
+++ server/src/core/abstracts/repositories/email-verification.repository.abstract.ts
@@ -1,20 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { EmailVerificationEntity } from 'src/core/entities';
+import { GenericRepositoryAbstract } from './generic.repositoty.abstract';
 
 @Injectable()
-export abstract class EmailVerificationRepositoryAbstract {
-    //#region Base repository   
-
-    abstract get(): Promise<EmailVerificationEntity[]>;
-
-    abstract getById(id: number): Promise<EmailVerificationEntity>;
-
-    abstract createOrUpdate(emailVerificationEntity: EmailVerificationEntity): Promise<EmailVerificationEntity>;
-
-    abstract delete(id: number): Promise<number>;
-
-    //#endregion
-
+export abstract class EmailVerificationRepositoryAbstract extends GenericRepositoryAbstract<EmailVerificationEntity> {
     abstract getPreviousEmailValidation(email: string): Promise<EmailVerificationEntity>;
 
     abstract getLatestEmailVerificationByUserId(userId: number, code: string): Promise<EmailVerificationEntity>;
