d4808e73f801c0bbe40b08aecf4d36d6cba0fa64
Andrija Eremic <andrija.eremic@gmail.com>
2023-06-09 21:15:12 +0200

    minor improvements

--- client/src/Components/Admin/Subjects/Content/Subject/Content/index.tsx
+++ client/src/Components/Admin/Subjects/Content/Subject/Content/index.tsx
@@ -1,20 +1,51 @@
-import { Card, CardContent, Container, Grid, Typography } from "@mui/material";
-import { Subject } from "../../../../../../resources/Typography";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { Add, Subject } from "../../../../../../resources/Typography";
 
 const Content = () => {
   return (
-    <Container sx={{ mt: 2 }}>
+    <Container sx={{ mt: 1 }}>
       <Typography variant="h5">{Subject}</Typography>
       <Grid container spacing={2}>
         <Grid item xs={4}>
-          <Card>
+          <Card sx={{ mt: 1 }}>
             <CardContent></CardContent>
           </Card>
         </Grid>
         <Grid item xs={8}>
-          <Card>
-            <CardContent></CardContent>
-          </Card>
+          <Stack>
+            <Card sx={{ mt: 1 }}>
+              <CardContent></CardContent>
+            </Card>
+            <Card sx={{ mt: 1 }}>
+              <CardContent></CardContent>
+            </Card>
+            <Card sx={{ mt: 1 }}>
+              <CardContent></CardContent>
+            </Card>
+            <Card sx={{ mt: 1 }}>
+              <CardContent></CardContent>
+            </Card>
+          </Stack>
+          <Stack>
+            <Box sx={{ mt: 1, textAlign: "right" }}>
+              <Button
+                variant="contained"
+                color="success"
+                sx={{ maxWidth: "xs" }}
+              >
+                {Add}
+              </Button>
+            </Box>
+          </Stack>
         </Grid>
       </Grid>
     </Container>
