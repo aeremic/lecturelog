a35e1cb660915a0113d6bde30aaf2073ba0d3a2b
Andrija Eremic <andrija.eremic@gmail.com>
2023-10-15 19:44:24 +0200

    added coments for jpath builder

--- server/src/core/common/jpath-query.builder.ts
+++ server/src/core/common/jpath-query.builder.ts
@@ -1,21 +1,50 @@
+/**
+ * Builder for constructing JPath query. Please note that calling addRoot method is mandatory before other methods calls.
+ * @returns JPath query
+ */
 export const JPathQueryBuilder = function () {
   return {
+    /**
+     * Method for adding root operator.
+     * @returns
+     */
     addRoot: function () {
       this.query = '$';
       return this;
     },
+    /**
+     * Method for adding child operator.
+     * @returns
+     */
     addChildOperator: function () {
       this.query += '.';
       return this;
     },
+    /**
+     * Method for adding element.
+     * @param element Element as a string
+     * @returns
+     */
     addElement: function (element: string) {
       this.query += element;
       return this;
     },
+    /**
+     * Method used for subscript filter matching.
+     * @param key Key for matching
+     * @param value Value to be match as a number
+     * @returns
+     */
     addSubscriptMatching: function (key: string, value: number) {
       this.query += `[?(@.${key}==${value})]`;
       return this;
     },
+    /**
+     * Method used for subscript filter matching.
+     * @param key Key for matching
+     * @param value Value to be match as a string
+     * @returns
+     */
     addSubscriptMatchingWithString: function (key: string, value: string) {
       this.query += `[?(@.${key}=="${value}")]`;
       return this;
