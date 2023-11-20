2339a38bf1f83f25e26b8756cf1e5b271c5f4896
Andrija Eremic <andrija.eremic@gmail.com>
2023-11-20 19:33:41 +0100

    fix

--- server/src/services/external-cache/redis.service.ts
+++ server/src/services/external-cache/redis.service.ts
@@ -3,46 +3,26 @@ import { Injectable } from '@nestjs/common';
 import { ExternalCacheSevice } from './external-cache.service';
 import { RedisCommands } from './redis.constant';
 import Redis from 'ioredis';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const ReadWriteLock = require('rwlock');
+import { Mutex } from 'async-mutex';
 
 @Injectable()
 export class RedisService implements ExternalCacheSevice {
-  private rwLock = null;
+  private mutex = null;
 
   constructor(@InjectRedis() private readonly redis: Redis) {
-    this.rwLock = new ReadWriteLock();
+    this.mutex = new Mutex();
   }
 
-  get(id: string, path: string = undefined): Promise<any> {
+  async get(id: string, path: string = undefined): Promise<any> {
     let result = undefined;
     const redisInstance = this.redis;
 
     if (id) {
-      this.rwLock.readLock(function (release: () => void) {
+      await this.mutex.runExclusive(async () => {
         if (path) {
-          result = redisInstance
-            .call(RedisCommands.GET, id, path)
-            .then((res: any) => {
-              release();
-              return res;
-            })
-            .catch(() => {
-              release();
-              return undefined;
-            });
+          result = await redisInstance.call(RedisCommands.GET, id, path);
         } else {
-          result = redisInstance
-            .call(RedisCommands.GET, id)
-            .then((res: any) => {
-              release();
-              return res;
-            })
-            .catch(() => {
-              release();
-              return undefined;
-            });
+          result = await redisInstance.call(RedisCommands.GET, id);
         }
       });
     }
@@ -50,44 +30,31 @@ export class RedisService implements ExternalCacheSevice {
     return result;
   }
 
-  set(id: string, object: any): Promise<any> {
+  async set(id: string, object: any): Promise<any> {
     let result = undefined;
     const redisInstance = this.redis;
 
     if (id) {
-      this.rwLock.writeLock(function (release: () => void) {
-        result = redisInstance
-          .call(RedisCommands.SET, id, '$', JSON.stringify(object))
-          .then((res: any) => {
-            release();
-            return res;
-          })
-          .catch(() => {
-            release();
-            return undefined;
-          });
+      await this.mutex.runExclusive(async () => {
+        result = await redisInstance.call(
+          RedisCommands.SET,
+          id,
+          '$',
+          JSON.stringify(object),
+        );
       });
     }
 
     return result;
   }
 
-  delete(id: string): Promise<any> {
+  async delete(id: string): Promise<any> {
     let result = undefined;
     const redisInstance = this.redis;
 
     if (id) {
-      this.rwLock.writeLock(function (release: () => void) {
-        result = redisInstance
-          .call(RedisCommands.DELETE, id)
-          .then((res) => {
-            release();
-            return res;
-          })
-          .catch(() => {
-            release();
-            return undefined;
-          });
+      await this.mutex.runExclusive(async () => {
+        result = await redisInstance.call(RedisCommands.DELETE, id);
       });
     }
 
