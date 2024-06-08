2a840993023da8ffbb353ed5edb2b76a2c44197b
Andrija Eremic <andrija.eremic@gmail.com>
2024-06-08 16:28:28 +0200

    prittier

--- server/src/core/dtos/requests/email-registration.dto.ts
+++ server/src/core/dtos/requests/email-registration.dto.ts
@@ -1,23 +1,29 @@
-import { MinLength } from "@nestjs/class-validator";
-import { IsNotEmpty, IsNumber, IsPositive, IsString, MaxLength } from "class-validator";
+import { MinLength } from '@nestjs/class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MaxLength,
+} from 'class-validator';
 
 export class EmailRegistrationDto {
-    @IsNumber()
-    @IsPositive()
-    @IsNotEmpty()
-    public userId: number;
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  public userId: number;
 
-    @IsString()
-    @MinLength(5)
-    @MaxLength(5)
-    @IsNotEmpty()
-    public code: string;
+  @IsString()
+  @MinLength(5)
+  @MaxLength(5)
+  @IsNotEmpty()
+  public code: string;
 
-    @IsString()
-    @IsNotEmpty()
-    public password: string;
+  @IsString()
+  @IsNotEmpty()
+  public password: string;
 
-    @IsString()
-    @IsNotEmpty()
-    public repeatedPassword: string;
+  @IsString()
+  @IsNotEmpty()
+  public repeatedPassword: string;
 }
